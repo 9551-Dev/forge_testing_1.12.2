14d6bd97ef019b6cf28249ffaac58324caf8c43b
9551Dev <oliverekcaha@gmail.com>
2023-01-28 21:01:35 +0100

    made shit not crash when hitting entities

--- src/main/java/com/example/examplemod/common/entity/TestEggEntity.java
+++ src/main/java/com/example/examplemod/common/entity/TestEggEntity.java
@@ -1,18 +1,26 @@
 package com.example.examplemod.common.entity;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityEgg;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 
-public class TestEggEntity extends CustomThrowableEntity {
+public class TestEggEntity extends EntityEgg {
     public TestEggEntity(World level, EntityPlayer player) {
         super(level,player);
     }
 
     @Override
     protected void onImpact(RayTraceResult rayTraceResult) {
-        BlockPos hit = rayTraceResult.getBlockPos();
+
+        BlockPos hit = new BlockPos(0,0,0);
+
+        if(rayTraceResult.typeOfHit == RayTraceResult.Type.BLOCK) {
+            hit = rayTraceResult.getBlockPos();
+        } else if(rayTraceResult.typeOfHit == RayTraceResult.Type.ENTITY) {
+            hit = rayTraceResult.entityHit.getPosition();
+        }
 
         this.world.createExplosion(this.getThrower(), hit.getX(), hit.getY(), hit.getZ(), 5, true);
         this.setDead();
