c0eb5210449e0eaff0f8d15cd018d3f496f1682a
9551Dev <oliverekcaha@gmail.com>
2023-02-02 20:55:13 +0100

    removed extra import

--- src/main/java/com/example/examplemod/common/items/GunPowderItemBehavior.java
+++ src/main/java/com/example/examplemod/common/items/GunPowderItemBehavior.java
@@ -3,7 +3,6 @@ package com.example.examplemod.common.items;
 import com.example.examplemod.ExampleMod;
 import com.example.examplemod.init.ModItems;
 import com.example.examplemod.tools.IHasModel;
-import com.example.examplemod.tools.ItemBase;
 import net.minecraft.item.ItemFood;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
